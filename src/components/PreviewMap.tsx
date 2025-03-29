75180043af16397f77c6525bb66d9796b4b2bd40
StoyanChorbov <slazy06bg@gmail.com>
2025-03-29 13:21:56 +0200

    Added working map
    # Conflicts:
    #	package-lock.json
    #	package.json
    #	src/index.css

--- src/components/PreviewMap.tsx
+++ src/components/PreviewMap.tsx
@@ -1,8 +1,29 @@
+import './preview-map.css';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import mapboxgl from 'mapbox-gl';
+import {useEffect, useRef} from "react";
+
 export default function PreviewMap() {
+    const mapRef = useRef<mapboxgl.Map | null>(null);
+    const mapContainerRef = useRef<HTMLDivElement>(HTMLDivElement.prototype);
+
+    useEffect(() => {
+        mapboxgl.accessToken = 'pk.eyJ1IjoiYWxla3NuaWtvbG92MiIsImEiOiJjbTh1MDduZGswNWRhMmtxd2V5M281OGtvIn0.IAp5PAdsbQ0WBbyLCF7dDA';
+
+        mapRef.current = new mapboxgl.Map({
+            container: mapContainerRef.current,
+            center: [23, 42],
+            zoom: 3
+        });
+
+        return () => {
+            mapRef.current?.remove();
+        }
+    }, []);
+
     return (
-        <div>
-            <h2>Preview Map</h2>
-            <p>Map will be displayed here</p>
+        <div id='preview-map-container' ref={mapContainerRef}>
+            <p>Bastun</p>
         </div>
     )
 }
