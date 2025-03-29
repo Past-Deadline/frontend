c37833b3d929512b6b84e6c2765fa9df731f402b
Nikola Velikov <niki.velikov09@gmail.com>
2025-03-29 17:14:06 +0200

    in progress

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,25 @@ import Header from "./components/Header.tsx";
 import Footer from "./components/Footer.tsx";
 import Slider from './components/Slider.tsx';
 import Checkbox from './components/checkbox.tsx';
+import  RangeSlider from './components/RangeSlider.tsx';
+import VerticalDoubleSlider from './components/RangeSlider.tsx';
+import { useState } from 'react';
 // import {RangeSlider} from './components/RangeSlider.tsx';
 function App() {
+  const [values, setValues] = useState<[number, number]>([30, 70]);
   return (
     <>
     
         <Header />
         <main>
           {/* <Slider/> */}
-          {/* <RangeSlider/> */}
+        <VerticalDoubleSlider
+          min={0}
+          max={100}
+          initialValues={values}
+          onChange={setValues}
+          height={400}
+        />
           {/* <Checkbox/> */}
         </main>
         <Footer />
--- src/components/RangeSlider.tsx
+++ src/components/RangeSlider.tsx
@@ -1,40 +1,45 @@
-import React, { useState } from 'react';
-import Nouislider from 'nouislider-react';
-import 'nouislider/distribute/nouislider.css';
+import React from 'react';
+import NouiSlider  from 'nouislider-react';
+import 'nouislider/dist/nouislider.css';
 
-export function NoUISliderComponent() {
-  const [minValue, setMinValue] = useState('30');
+interface VerticalDoubleSliderProps {
+  min: number;
+  max: number;
+  initialValues: [number, number];
+  step?: number;
+  onChange: (values: [number, number]) => void;
+  className?: string;
+  height?: number;
+}
 
-  // This function will handle the slider change
-  const displayMin = (event: number[]) => {
-    setMinValue(event[0].toString());
+const VerticalDoubleSlider: React.FC<VerticalDoubleSliderProps> = ({
+  min,
+  max,
+  initialValues,
+  step = 1,
+  onChange,
+  className = '',
+  height = 300,
+}) => {
+  const handleChange = (values: string[]) => {
+    const numericValues: [number, number] = [parseFloat(values[0]), parseFloat(values[1])];
+    onChange(numericValues);
   };
 
   return (
-    <div>
-      {/* Slider Component */}
-      <Nouislider
-        range={{ min: 0, max: 100 }}
-        start={[30, 80]}
+    <div className={`vertical-slider-container ${className}`} style={{ height: `${height}px` }}>
+      <NouiSlider
+        range={{ min, max }}
+        start={initialValues}
+        step={step}
+        orientation="vertical"
         connect
-        onChange={displayMin}
+        behaviour="drag"
+        tooltips={false}
+        onChange={handleChange}
       />
-      <center>
-        <div style={{ display: 'inline', padding: '2%' }}>
-          <h3>Min Value</h3>
-          <br />
-          <div
-            style={{
-              background: 'green',
-              color: 'white',
-              display: 'inline',
-              padding: '1%',
-            }}
-          >
-            {minValue}
-          </div>
-        </div>
-      </center>
     </div>
   );
-}
+};
+
+export default VerticalDoubleSlider;
