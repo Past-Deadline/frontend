4a95f702ef601884ec97c9ad9c4cfea6a1dd8241
Nikola Velikov <niki.velikov09@gmail.com>
2025-03-30 01:29:23 +0200

    add new page

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import './index.css'
 import App from './App.tsx'
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import { PreviewPage } from './pages/PreviewPage.tsx';
+import { MapFormPage } from './pages/MapFormPage.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <Router>
       <Routes>
         <Route index Component={App} />
         <Route path="/preview" Component={PreviewPage} />
+        <Route path="/scheduler" Component={MapFormPage} />
       </Routes>
   </Router>
 )
