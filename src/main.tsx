7e83d95b4b42650ca8fe8e670f19ff76f1745ea7
StoyanChorbov <slazy06bg@gmail.com>
2025-03-29 13:21:56 +0200

    Added working map

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import PreviewMap from "./components/PreviewMap.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <Router>
-      <Route path="/" Component={App} />
+      <Routes>
+        <Route index Component={App} />
+        <Route path="/map" Component={PreviewMap} />
+      </Routes>
   </Router>
 )
